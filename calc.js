afd3e6981b4080285d1ca8d310900ed676c79242
Steph Lee <stsmlee@gmail.com>
2023-06-15 12:29:20 -0400

    addition works

--- calc.js
+++ calc.js
@@ -7,22 +7,40 @@ let gotDecimal = false;
 const buttons = document.querySelectorAll('button');
 buttons.forEach((btn) => {
     btn.addEventListener('click', () => {
-        if (btn.className == 'number') updateNum(btn.textContent);
+        if (btn.className == 'number') updateDisplay(btn.textContent);
         else if (btn.id == 'decimal' && !gotDecimal) {
-            updateNum('.');
+            updateDisplay('.');
             gotDecimal = true;
         } else if (!lastNum) {
             lastNum=currentNum;
             display.textContent += btn.textContent;
+            currentOperator = btn.id;
+            currentNum = 0;
+            gotDecimal = false;
+        } else {
+            const result = calculate(lastNum,currentNum,currentOperator);
+            display.textContent = result;
+            lastNum = 0;
+            currentNum = result;
+            gotDecimal = false;
         };
     });
 });
 
-function updateNum(n) {
-    if (!currentNum) {
+function updateDisplay(n) {
+    if (currentNum==0 && !lastNum) {
         currentNum = n;
         display.textContent = currentNum;
     } else {
         currentNum += n;
         display.textContent += n;
     }};
+
+function calculate(n1, n2, op) {
+    n1 = +n1;
+    n2 = +n2;
+    switch(op) {
+        case 'add':
+            return n1 + n2
+    };
+};
